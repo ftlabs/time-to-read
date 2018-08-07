9898d05e653f63e194b9e45ce8296ae94ad7b552
Jamie.Brown <jamiebrown201@hotmail.com>
2018-08-07 11:08:38 +0100

    add multiple text inputs

--- handler.js
+++ handler.js
@@ -6,26 +6,32 @@ const article = require("./helpers/article");
 
 module.exports.main = async (event, context, callback) => {
   const data = JSON.parse(event.body);
-  let text = data.input === undefined ? "" : data.input;
+  if (typeof data.textInput === "array") data.textInput.join(" ");
+  let text = data.textInput || "";
+  const articles = data.articles || [];
 
-  if (data.uuid) {
-    try {
-      const articleText = await article.getArticleText(data.uuid);
-      text = `${text} ${articleText}`;
-    } catch (error) {}
-  }
+  Promise.all(articles.map(uuid => article.getArticleText(uuid)))
+    .then(results => {
+      text = `${text} ${results.join(" ")}`;
+      const wordCount = text.split(" ").length;
+      let timeToRead = timeToReadModule.inMinutes(wordCount, data.readingSpeed);
 
-  const wordCount = text.split(" ").length;
-  let timeToRead = timeToReadModule.inMinutes(wordCount, data.readingSpeed);
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          minutes: timeToRead,
+          normalized: timeNormalizer.minute(timeToRead),
+          input: event
+        })
+      };
 
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      minutes: timeToRead,
-      normalized: timeNormalizer.minute(timeToRead),
-      input: event
+      callback(null, response);
     })
-  };
-
-  callback(null, response);
+    .catch(error => {
+      const response = {
+        statusCode: 500,
+        error
+      };
+      callback(null, response);
+    });
 };
